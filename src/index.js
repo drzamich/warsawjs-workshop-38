2b793dbcdfa45837923c451897392c002b81a4de
Michal Drzazga <mdrzazga@microstrategy.com>
2019-11-24 15:52:00 +0100

    retry on error; information about time since last fetch

--- src/index.js
+++ src/index.js
@@ -2,16 +2,23 @@ require('./index.css')
 
 const { getPosts } = require('./api')
 const { renderPosts, renderStatus } = require('./ui')
-const { combineLatest, timer, fromEvent } = require('rxjs');
-const { switchMap, map, tap, startWith, share } = require('rxjs/operators');
+const { combineLatest, timer, fromEvent, EMPTY } = require('rxjs');
+const { switchMap, map, tap, startWith, share, retry, retryWhen, delay, catchError } = require('rxjs/operators');
+
 
 const posts$ = timer(0, 5000).pipe(
   switchMap(() => getPosts()),
   tap(posts => console.log('Posts: ', posts)),
-  share(),
   tap({
     error: err => console.log('ERROR!', err)
   }),
+  // catchError(error => { renderStatus(error); return EMPTY}),
+  retryWhen(error$ => error$.pipe(
+    tap(() => console.log('Before delay')),
+    delay(1000)),
+    tap(() => console.log('After delay')),
+  ),
+  share(),
 );
 
 const filter$ = fromEvent(document.getElementById('filter'), 'input').pipe(
@@ -25,12 +32,10 @@ const filteredPosts$ = combineLatest(posts$, filter$).pipe(
 )
 
 const status$ = posts$.pipe(
-  map(posts => `Fetched ${posts.length} posts.`)
+  switchMap((posts) => timer(0, 1000).pipe(
+    map(time => `Fetched ${posts.length} posts ${time}s ago.`)
+  ))
 );
-
-const errorStatus$ = posts$.subscribe({
-  error: err => renderStatus(err)
-})
 
 filteredPosts$.subscribe(renderPosts)
 status$.subscribe(renderStatus)
