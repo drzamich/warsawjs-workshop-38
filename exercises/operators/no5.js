6e5357679f60d203fd7f39fe17d6307c659d80d7
Michal Drzazga <mdrzazga@microstrategy.com>
2019-11-24 17:58:31 +0100

    scan + pairwise

--- exercises/operators/no5.js
+++ exercises/operators/no5.js
@@ -1,8 +1,15 @@
-const { timer } = require('rxjs')
-const { scan } = require('rxjs/operators')
+const { timer, pipe } = require('rxjs')
+const { scan, pairwise } = require('rxjs/operators')
 
-const price$ = timer(0, 700).pipe(
+const price$ = timer(0, 1000).pipe(
   scan((acc) => Math.round(acc + Math.random() * 60 - 30), 150),
 )
 
-price$.subscribe(console.log)
+const priceChanges$ = price$.pipe(
+  pairwise(),
+  scan((acc, cur) => {
+    const difference = Math.abs(cur[1]-cur[0])
+    return acc + difference
+  }, 0)
+)
+priceChanges$.subscribe(console.log)
