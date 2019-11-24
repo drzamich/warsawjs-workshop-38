0f14b304b2a5fb8c2e2d9e54047d08920cdddc9e
Michal Drzazga <mdrzazga@microstrategy.com>
2019-11-24 17:36:15 +0100

    pairwise

--- exercises/operators/no4.js
+++ exercises/operators/no4.js
@@ -1,12 +1,22 @@
-const { timer } = require("rxjs");
-const { scan } = require('rxjs/operators')
+const { timer, pipe } = require("rxjs");
+const { pairwise, startWith, scan, map, filter } = require('rxjs/operators')
 
 const names = ['Tom', 'Bob', 'Alice', 'Peter']
 
+let id = 0;
 const posts$ = timer(0, 1000).pipe(
-  scan(acc => [...acc, {
-    id: Math.round(Math.random() * 10),
-    author: names[Math.round(Math.random() * 3.9)]
-  }], [])
+  startWith([]),
+  scan(acc => Math.random() > 0.8 ? [...acc, {
+    id: id++,
+    author: names[Math.round(Math.random() * 2.9)]
+  }] : acc, []),
+  pairwise(),
+  map(([before, after]) => {
+    if(after.length !== before.length) {
+      return after[after.length - 1].author;
+    }
+  }),
+  filter(author => author !== undefined)
 )
 
+posts$.subscribe(console.log)
